8caaae39bbeff02a2a682466e8ea376751341642
austinb847 <austinb847@icloud.com>
2020-05-26 16:10:25 -0700

    add views for showing all albums, and showing individual album

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'albums#index'
+  root to: 'albums#index' #root route
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :albums do # resources keyword creates all the routes needed to add CRUD functionality for that classs
     resources :songs #nest song routes with albums
